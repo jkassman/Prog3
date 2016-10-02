0269f0e6f077544042e3ff8dadb43f5c776b7b04
Matt Paulson <mpaulson@nd.edu>
2016-10-02 18:13:18 -0400

    Added cool messages to send to client

--- server/myftpd.c
+++ server/myftpd.c
@@ -11,6 +11,7 @@
 int main(int argc, char * argv[]){
   struct sockaddr_in sin;
   char buf[4096];
+  char message[4096];
   char *portString;
   socklen_t len;
   int s, new_s, port, sent;
@@ -33,24 +34,24 @@ int main(int argc, char * argv[]){
 
   /*setup passive open*/
   if((s=socket(PF_INET,SOCK_STREAM,0))<0){
-    perror("simplex-talk:socket");
+    perror("myftpd:socket");
     exit(1);
   }
   //bind socket to specified address
   if((bind(s,(struct sockaddr *)&sin,sizeof(sin)))<0){
-    perror("simplex-talk: bind");
+    perror("myftpd: bind");
     exit(1);
   }
   //listen to the socket
   if((listen(s,0))<0){
-    perror("simplex-talk:listen");
+    perror("myftpd:listen");
     exit(1);
   }
   printf("Hello, and Welcome to the Server of the 21st Century!\n");
   /*wait for connection, then receive and print text */
   while(1){
     if((new_s = accept(s,(struct sockaddr *)&sin,&len))<0){
-      perror("simplex-talk:accept");
+      perror("myftpd:accept");
       exit(1);
     }
     while(1){
@@ -59,13 +60,42 @@ int main(int argc, char * argv[]){
         exit(1);
       }
       if(len==0) break;
+      if(strcmp("REQ",buf)==0){
+        strcpy(message,"You sent REQ! Congrats lol\n");
+      }
+      else if(strcmp("UPL",buf)==0){
+        strcpy(message,"You sent UPL! ay lmao\n");
+      }
+      else if(strcmp("DEL",buf)==0){
+        strcpy(message,"You sent DEL! w00t!\n");
+      }
+      else if(strcmp("LIS",buf)==0){
+        strcpy(message,"You sent LIS! Booyah!\n");
+      }
+      else if(strcmp("MKD",buf)==0){
+        strcpy(message,"You sent MKD! Way to go man!\n");
+      }
+      else if(strcmp("RMD",buf)==0){
+        strcpy(message,"You sent RMD! What a play!\n");
+      }
+      else if(strcmp("CHD",buf)==0){
+        strcpy(message,"You sent CHD! Boomshakalaka!\n");
+      }
+      else if(strcmp("XIT",buf)==0){
+        close(new_s);
+        break;
+      }
+      else{
+        strcpy(message,"Send a correct command dumbass\n");
+      }
       printf("TCP Server Received:%s\n",buf);
-      if((sent=send(new_s,buf,len,0))==-1){
+      if((sent=send(new_s,message,strlen(message),0))==-1){
         perror("Server Send Error!");
         exit(1);
       }
     }
-    printf("Client finishes, close the connection!\n");
+    printf("Client Quit!\n");
     close(new_s);
   }
+  close(s);
 }
