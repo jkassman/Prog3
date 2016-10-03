3c440322df06d87cc92f30f29f5175ec1fb11efb
jkassman <jkassman@nd.edu>
2016-10-03 00:32:08 -0400

    Request is almost working.

--- client/myftp.c
+++ client/myftp.c
@@ -59,10 +59,17 @@ void sendOpCode(const char *opcode, int sock)
 int clientRequest(int sock)
 {
     sendOpCode("REQ", sock);
-    //Receive Message from the server
-    char recvBuffy[PROG3_BUFFER_SIZE];
+    //Receive query from the server
+    char recvQuery[PROG3_BUFFER_SIZE];
+    unsigned short int fileNameLen;
+    unsigned short int fileNameLenToSend;
+    char fileName[1000];
+    unsigned char hash[16];
+    unsigned int fileLenBuffy;
+    int status;
     int bytesRecvd;
-    bytesRecvd = recv(sock, recvBuffy, sizeof(recvBuffy), 0);
+    int fileLenRecvd, hashRecvd, fileRecvd;
+    bytesRecvd = recv(sock, recvQuery, sizeof(recvQuery), 0);
     if (bytesRecvd < 0)
     {
         fprintf(stderr, "myftp Error: recv(): %s\n", strerror(errno));
@@ -70,14 +77,85 @@ int clientRequest(int sock)
         exit(3);
     }
 
-    //print out raw bytes received
-    printf("Got %d bytes\n", bytesRecvd);
+    //print out received query:
     int i;
     for (i = 0; i < bytesRecvd; i++)
     {
-        printf("%c", recvBuffy[i]);
+        printf("%c", recvQuery[i]);
     }
-    puts("");
+    puts(""); 
+
+    //Sends file name and length:
+    fgets(fileName, 1000, stdin);
+    fileNameLen = strlen(fileName);
+    fileName[(strlen(fileName)-1)] = '\0';
+    fileNameLenToSend = htons(fileNameLen);
+
+    status = send(sock, (char *)&fileNameLenToSend,2, 0);
+    if (status < 0)
+    {
+        fprintf(stderr, "myftp error sending file name length for REQ: %s\n", strerror(errno));
+        close(sock);
+        exit(3);
+    }
+    sendOpCode(fileName, sock);
+    
+    //Receives 32-bit file length:
+    fileLenRecvd = recv(sock, (char *)&fileLenBuffy, 4, 0);
+    if(fileLenRecvd < 0) 
+    {
+       fprintf(stderr, "myftp Error: recv() 32-bit file length: %s\n", strerror(errno));
+        close(sock);
+        exit(3);
+    }
+
+    printf("File length: %i \n", fileLenBuffy);
+
+    //Decode 32-bit value:
+    if(fileLenBuffy == -1) {
+      printf("File does not exist on server.\n");
+      return 0;
+    }
+
+    //Receives MD5 Hash:
+    hashRecvd = recv(sock, hash, 16, 0);
+    if(hashRecvd < 0) 
+    {
+       fprintf(stderr, "myftp Error: recv() MD5 hash: %s\n", strerror(errno));
+        close(sock);
+        exit(3);
+    }
+  
+    int j;
+    printf("Hash value: \n");
+    for( j = 0; j < 16; j++) {
+      printf("%02x.", hash[j]);
+    }
+    printf("\n");
+
+    //Reads x number of bytes from server:
+    char file[fileLenBuffy];
+    int counter = 0;
+    FILE *f = fopen(fileName, "w");
+    if(!f){
+     printf("Error opening file \n");
+     return 0;
+    }
+    while(counter < fileLenBuffy){
+      fileRecvd = recv(sock, file, fileLenBuffy, 0);
+      if(fileRecvd < 0) 
+      {
+       fprintf(stderr, "myftp Error: recv() file from server: %s\n", strerror(errno));
+        close(sock);
+        exit(3);
+      } 
+      fwrite(file, 1, fileRecvd, f);  //write into file f and save
+      counter = counter + fileRecvd;
+    }
+    fclose(f);
+
+    //Computes the MD5 hash of recieved file: 
+
     return 0;
 }
 
